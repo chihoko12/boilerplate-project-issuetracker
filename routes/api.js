4fbf5005a1919a07a98ad3749fa130f0b92df2ec
cu <harrierraum@hotmail.com>
2024-03-08 10:10:07 +0100

    fix GET method

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@
 const { ObjectId } = require('mongodb');
 const myDB = require('../connection');
 
+// initialize database connection
 let myDataBase;
 
 myDB().then(client => {
@@ -27,20 +28,20 @@ module.exports = function (app) {
 
       // construct the filter object based on query parameters
       const filter = {};
-      if (query.open) {
-        filter.open = query.open === 'true';
-      }
-      if (query.assigned_to) {
-        filter.assigned_to = query.assigned_to;
-      }
-
-      if (query.issue_title) {
-        filter.issue_title = query.issue_title;
-      }
-
-      if (query.status_text) {
-        filter.status_text = query.status_text;
-      }
+      Object.keys(query).forEach(key => {
+        if (query[key]) {
+          // add non-empty query parameteres to the filter
+          if (key === '_id') {
+            // conver _id to ObjectId
+            filter[key] = ObjectId(query[key]);
+          } else if (key === 'open') {
+            // convert 'open' to boolean
+            filter[key] = query[key] === 'true';
+          } else {
+            filter[key] = query[key];
+          }
+        }
+      });
 
       // fetch issued from the database based on projects and filter
       myDataBase.collection(project).find(filter).toArray(function(err,issues) {
@@ -64,7 +65,7 @@ module.exports = function (app) {
 
       // validate required fields
       if (!issue_title || !issue_text || !created_by) {
-        return res.status(400).json({ error: 'required field(s) missing' })
+        return res.json({ error: 'required field(s) missing' })
       }
 
       // create a new issue object
@@ -83,14 +84,14 @@ module.exports = function (app) {
       // insert the new issue into the database
       myDataBase.collection(project).insertOne(newIssue, function(err,result) {
         if (err) {
-          res.status(400).json({ error: 'could not create an issue' });
+          res.json({ error: 'could not create an issue' });
         } else {
           res.json(result.ops[0]);
         }
       });
     })
 
-    .put(function (req, res){
+    .put(async function (req, res){
       let project = req.params.project;
       const {
         _id,
@@ -99,53 +100,55 @@ module.exports = function (app) {
         created_by,
         assigned_to,
         status_text,
+        open
       } = req.body;
 
       // validate _id
       if (!_id || !ObjectId.isValid(_id)) {
-        return res.status(400).json({ error: 'missing _id' });
+        return res.json({ error: 'missing _id' });
       }
 
+      // check if any update fields(s) are sent
       if (!issue_title && !issue_text && !created_by && !assigned_to && !status_text ) {
-        return res.status(400).json({ error: 'no update field(s) sent', '_id': _id  })
+        return res.json({ error: 'no update field(s) sent', '_id': _id  })
       }
 
       // construct the update object
-      const updateFields = {};
-      if (issue_title) {
-        updateFields.issue_title = issue_title;
-      }
+      try {
+        // retrieve project data
+        const projectData = await myDataBase.collection(project).findOne({ _id: ObjectId(_id) });
 
-      if (issue_text) {
-        updateFields.issue_text = issue_text;
-      }
-
-      if (created_by) {
-        updateFields.created_by = created_by;
-      }
-
-      if (assigned_to) {
-        updateFields.assigned_to = assigned_to;
-      }
-
-      if (status_text) {
-        updateFields.status_text = status_text;
-      }
-
-      updateFields.updated_on = new Date();
-
-      // update the issue in the database
-      myDataBase.collection(project).updateOne({ _id: ObjectId(_id)}, { $set: updateFields }, function(err, result) {
-        if (err) {
-          res.status(400).json({ error: 'could not update', '_id': _id });
-        } else {
-          if (result.matchedCount === 0) {
-            res.status(400).json({ error: 'could not update', '_id': _id });
-          } else {
-            res.json({ result: 'successfully updated', '_id': _id });
-          }
+        // check if project exits
+        if (!projectData) {
+          return res.json({ error: "could not update", _id: _id });
         }
-      });
+
+        // construct update object
+        const updateFields = {};
+        if (issue_title) updateFields.issue_title = issue_title;
+        if (issue_text) updateFields.issue_text = issue_text;
+        if (created_by) updateFields.created_by = created_by;
+        if (assigned_to) updateFields.assigned_to = assigned_to;
+        if (status_text) updateFields.status_text = status_text;
+        if (typeof open === 'boolean') updateFields.open = open;
+        updateFields.updated_on = new Date();
+
+        // update the issue in the database
+        const result = await myDataBase.collection(project).updateOne(
+          { _id: ObjectId(_id) },
+          { $set: updateFields }
+        );
+
+        // check if the update was successful
+        if (result.modifiedCount === 1) {
+          return res.json({ result: 'successfully updated', '_id': _id });
+        } else {
+          return res.json({ error: 'could not update', _id: _id });
+        }
+      } catch(e) {
+        console.error(e);
+        return res.json({ error: 'could not update', _id: _id });
+      }
     })
 
     .delete(function (req, res){
@@ -154,16 +157,16 @@ module.exports = function (app) {
 
       //validate _id
       if (!_id || !ObjectId.isValid(_id)) {
-        return res.status(400).json({ error: 'missing _id' });
+        return res.json({ error: 'missing _id' });
       }
 
       // delete the issue from the database
       myDataBase.collection(project).deleteOne({ _id: ObjectId(_id) }, function(err, result) {
         if (err) {
-          res.status(400).json({ error: 'could not delete', '_id': _id });
+          res.json({ error: 'could not delete', '_id': _id });
         } else {
           if (result.deletedCount === 0) {
-            res.status(400).json({ error: 'could not delete', '_id': _id });
+            res.json({ error: 'could not delete', '_id': _id });
           } else {
             res.json({ result: 'successfully deleted', '_id': _id });
           }
